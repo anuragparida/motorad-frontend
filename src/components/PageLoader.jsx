74f81fb2bb23efd26ad4e7662e6cafbd35a665fc
Nikul <=>
2021-11-24 14:42:25 +0530

    changes

--- src/components/PageLoader.jsx
+++ src/components/PageLoader.jsx
@@ -1,15 +1,22 @@
 import { Backdrop, CircularProgress } from '@material-ui/core';
+import LinearProgress from '@mui/material/LinearProgress';
 
 
 export default function PageLoader(props) {
-    return (
-        <>
-        <Backdrop
-        sx={{ background: "white", color: '#10b068', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+  return (
+    <>
+      {props.loader ? <LinearProgress color="success" style={{ zIndex: "99999999" }} />
+        :
+        ""}
+
+
+      <Backdrop
+        sx={{ background: "", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={props.loader}
       >
-        <CircularProgress color="inherit" />
+        {/* <CircularProgress color="inherit" /> */}
+
       </Backdrop>
-        </>
-    );
-  }
+    </>
+  );
+}
